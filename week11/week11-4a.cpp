7ab835a28623fcd6bd508edc1dffe21900820018
henrywei5666 <145088333+henrywei5666@users.noreply.github.com>
2024-11-20 11:40:08 +0800

    week11

---
+++ week11/week11-4a.cpp
@@ -0,0 +1,12 @@
+//week11-4a
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	while(cin >> n)
+	{
+		if(n==0) break;
+		cout << n << "\n";
+	}
+}
---
+++ week11/week11-4b.cpp
@@ -0,0 +1,23 @@
+//week11-4b
+#include <iostream>
+using namespace std;
+int f(int n)
+{
+	int ans=0;
+	while(n>0)
+	{
+		ans += n%10;
+		n /= 10;
+	}
+	return ans;
+}
+int main()
+{
+	int n;
+	while(cin >> n)
+	{
+		if(n==0) break;
+		n=f(f(f(n)));
+		cout << n << "\n";
+	}
+}
